a174ab470e86097c8c95cc000ade52631b2ca14d
Tanveer143s <ertanveer143@gmail.com>
2024-03-08 17:13:25 +0530

    feat- updated provider in version.tf

--- versions.tf
+++ versions.tf
@@ -3,10 +3,6 @@ terraform {
   required_version = ">= 1.6.6"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.31.0"
-    }
     google = {
       source  = "hashicorp/google"
       version = ">= 3.50, < 5.0"
